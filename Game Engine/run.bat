e248f9c91db3a17ecbcb7cd5a888c28763d9b1e4
jacob gilley <jacob.gilley@gmail.com>
2022-09-20 17:27:40 -0500

    fixed run.bat

--- Game Engine/run.bat
+++ Game Engine/run.bat
@@ -1,2 +1,1 @@
-cd  "C:\Development\Java\Projects\game-engine\Game Engine"
-java -cp "C:\Development\Java\Projects\game-engine\Game Engine\build\classes;C:\Development\Java\Libs\lwjgl\*" TestGame
+java -cp ".\build\classes;.\libs\lwjgl\*" TestGame
