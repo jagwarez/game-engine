5200e093e034944eff65e637a4f07576a587eada
jacob gilley <jacob.gilley@gmail.com>
2023-05-25 21:35:08 -0500

    Fixed batch script

--- Game Engine/run.bat
+++ Game Engine/run.bat
@@ -4,13 +4,13 @@ cd /D "%~dp0"
 if /i "%1"=="trace" goto trace
 if /i "%1"=="retrace" goto retrace
     
-java -cp ".\build\classes;.\libs\lwjgl\*" TestGame
+java -cp ".\build\classes;.\libs\lwjgl\*" HelloGame
 
 goto exit
 
 :trace
 
-apitrace trace --output=./trace/game.trace --api=gl java -cp ".\build\classes;.\libs\lwjgl\*" TestGame
+apitrace trace --output=./trace/game.trace --api=gl java -cp ".\build\classes;.\libs\lwjgl\*" HelloGame
 
 goto exit
 
